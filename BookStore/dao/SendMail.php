7575cb65edd91380439fa7071326b29d34636a77
loannt401 <looaann123@gmail.com>
2024-06-14 18:02:13 +0700

    customer

--- BookStore/dao/SendMail.php
+++ BookStore/dao/SendMail.php
@@ -20,23 +20,21 @@ class SendMail {
 
         try {
             //Server settings
-            $mail->SMTPDebug = 0;                      //Enable verbose debug output
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'webrapchieuphim@gmail.com';                     //SMTP username
-            $mail->Password   = 'oywo uaeh idse ywwc';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-            $mail->SMTPSecure = "tls";                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            $mail->SMTPDebug = 0;                      
+            $mail->isSMTP();                                  
+            $mail->Host       = 'smtp.gmail.com';           
+            $mail->SMTPAuth   = true;                        
+            $mail->Username   = '';                 
+            $mail->Password   = '';                           
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;      
+            $mail->Port       = 587;                                 
+            $mail->SMTPSecure = "tls";                               
             $mail->CharSet    = "UTF-8";
 
-            //Recipients
             $mail->setFrom('cuahangsach021@gmail.com', 'Sach021');
-            $mail->addAddress($address, $name);     //Add a recipient
+            $mail->addAddress($address, $name);   
 
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->isHTML(true);                         
             $mail->Subject = $title;
             $mail->Body    = $content;
 
